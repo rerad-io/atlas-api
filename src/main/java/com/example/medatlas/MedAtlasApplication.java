996c3740d76f1520e5f793c96adb6c7a5e02bc02
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-10-09 15:39:39 +0200

    Start structure 1.0

---
+++ src/main/java/com/example/medatlas/MedAtlasApplication.java
@@ -0,0 +1,13 @@
+package com.example.medatlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedAtlasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedAtlasApplication.class, args);
+    }
+
+}
