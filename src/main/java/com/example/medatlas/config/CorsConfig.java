5e5fa96702774c6ebfbf1db3d0ef17748597bfc1
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-10-23 18:04:36 +0200

    ATLAS-26.2.4 Create tests for API. (Controllers + Mappers tests), set up a DB-connection

---
+++ src/main/java/com/example/medatlas/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.example.medatlas.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Разрешаем все источники (*) - в продакшене лучше настроить разрешенные источники.
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
--- src/main/java/com/example/medatlas/config/SwaggerConfig.java
+++ src/main/java/com/example/medatlas/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,10 +14,8 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.medatlas"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.medatlas.controller"))
                 .paths(PathSelectors.any())
-                .build()
-                .pathMapping("/")
-                .useDefaultResponseMessages(false);
+                .build();
     }
 }
