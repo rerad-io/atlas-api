4f1e8741a232c87dbeb61b62712c0a1f121ef061
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-11-27 21:38:50 +0100

    ATLAS-43. Add complex search in AnatomicalStructureController with subjectId

--- src/main/java/com/example/medatlas/repository/AnatomicalStructureRepository.java
+++ src/main/java/com/example/medatlas/repository/AnatomicalStructureRepository.java
@@ -11,7 +11,6 @@ import java.util.UUID;
 
 @Repository
 public interface AnatomicalStructureRepository extends JpaRepository<AnatomicalStructure, UUID> {
-
     @Query("SELECT a FROM AnatomicalStructure a WHERE (:name is null or lower(a.name) LIKE lower(concat('%', :name, '%'))) AND (:anatomicalStructureSubjectId is null or a.anatomicalStructureSubject.id = :anatomicalStructureSubjectId) " +
             "ORDER BY " +
             "CASE WHEN :orderBy = 'name' AND :orderByDirection = 'asc' THEN a.name END ASC, " +
